d11640bcddc4dd65d57e24704f35cf1a2df373d3
bhatt359-beep <bhatt359@umn.edu>
2026-02-14 17:30:12 -0600

    Added ai integration with ollama models

--- main.py
+++ main.py
@@ -1,6 +1,77 @@
 from flask import Flask, render_template, jsonify, request
+from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import ChatOllama
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from tqdm import tqdm
+import pdf_loader
+import requests
+import os
+
 
 app = Flask(__name__)
+
+# Step 1: Create Vector Database
+
+raw_file_content = requests.get("https://www.helmerinc.com/sites/default/files/2026-01/Manual%20-%20GX%20Refrigerator%20IFU%20360414-E.pdf").content
+
+file_path = './manual.pdf'
+
+if not os.path.exists(file_path):
+    with open("manual.pdf", "wb") as f:
+        f.write(raw_file_content)
+
+file_text = pdf_loader.get_pdf_text(file_path)
+chunks = pdf_loader.split_text_into_chunks(file_text)
+
+embeddings = OllamaEmbeddings(model="nomic-embed-text:latest", num_gpu=8)
+
+documents = []
+
+for i in tqdm(range(len(chunks))):
+    document = Document(
+        page_content=chunks[i],
+        metadata={"source":"pdf"},
+        id=i
+    )
+    documents.append(document)
+
+persist_dir = "chroma_db"
+vector_store = Chroma.from_documents(documents, embeddings)
+app.logger.info("Vector Store Set Up")
+
+@app.route("/test")
+def test():
+    user_prompt = "What should I do to avoid an injury?"
+
+    retrieved_docs = vector_store.similarity_search(user_prompt)
+
+    ollama_model = "qwen2.5"
+
+    llm = ChatOllama(
+        model=ollama_model,
+        temperature=0.7,
+    )
+
+    context = ""
+
+    for i in range(len(retrieved_docs)):
+        context += f"Context {i}: \n " + retrieved_docs[i].page_content + "\n_____________________\n"
+
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant that teaches users how to use their GX Refrigerator.",
+        ),
+        ("human", context + "\n_____________________\n" + user_prompt),
+    ]
+    
+    ai_msg = llm.invoke(messages)
+    
+    return jsonify({
+        "response": ai_msg.content
+    })
+        
 
 
 if __name__ == '__main__':
